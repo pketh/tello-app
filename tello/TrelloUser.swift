474707784128e2a0816b2214acfa77edcb040154
Pirijan <pirijan@gmail.com>
2014-10-04 10:23:01 -0400

    committing steady state
    hits api properly, doesn’t do much with it still

--- tello/TrelloUser.swift
+++ tello/TrelloUser.swift
@@ -25,7 +25,6 @@ class TrelloUser {
 		self.getBoards()
 	}
 
-	//-> (Array<String>, Array<String>, String)
 	private func getBoards() {
 		let trelloMember = "https://api.trello.com/1/members/me"
 		let boardParams = [
@@ -33,43 +32,61 @@ class TrelloUser {
 			"key": trelloKey,
 			"token": token
 		]
-
-		// ?vars for boardIDs, boardNames, avatarHash
 		Alamofire.request(.GET, trelloMember, parameters: boardParams)
 			.response {(request, response, data, error) in
-				let json = JSONValue(data as NSData!)
-				println(json)
+				if (error != nil) {
+					println(request)
+					self.offline(error)
+				} else {
+					let json = JSONValue(data as NSData!)
+					println(json) // prints full json
 
-				println("🐸🐸🐸🐸")
-				let boardsJSON = json["boards"].array!
-				println(boardsJSON)
-				println(boardsJSON.count)
+					let jsonBoards = json["boards"].array! // boardid, boardname
+					println(jsonBoards.count) // returns 6. ERR - undefined before use
 
-//				println(json["boards"][0]["id"])
-
-
-				// 🔮parse out boardIds
-				// parse out boardName
-				// --> make this a DICT to pass to self.getBoardColors
-				// parse out , avatarHash = "fb4e49d1cc52a8c4da3e6cebb600f64c"
-				println("🐸 Async Frog Test")
-				self.getAvatar(json)
+					self.getBoardColors(jsonBoards)
+					// println(json["boards"][0]["id"])
+					// parse out boardName
+					// --> make this a DICT to pass to self.getBoardColors
+					// parse out , avatarHash = "fb4e49d1cc52a8c4da3e6cebb600f64c"
+					println("🐸 Async Frog Test")
+					self.getAvatar(json)
+				}
 			}
+	}
+	
+	private func getBoardColors(jsonBoards: Array<JSONValue>) { //? needs type: Array
+		println(jsonBoards)
+		let trelloBoard = "https://api.trello.com/1/boards/" // + boardID/prefs
+		let boardParams = [
+			"key": trelloKey,
+			"token": token
+		]
+		for board in jsonBoards {
+			let boardID = "..."
+			println("🎆")
+			println(board)
+			// - hit the api w alamofire
+			// - parse
+		}
+
+		// test url = https://api.trello.com/1/boards/(BOARDID)/prefs?key=8240d71e24f95848cb9ca146bc84ede7&token=b9ae1a45713ce5f55bd14d83c96825367c67a5febf6386e39c878953ef5d6563
+		// let trelloBoardColorURL = "https://api.trello.com/1/boards/\(boardID)/prefs?key=\(key)&token=\(user)"
+		// has all logic/processing to store color / custom bk imag into model
 	}
 
 	private func getAvatar(json: JSONValue) {
 		let avatarHash = json["avatarHash"]
 		println("avatar hash is \(avatarHash)")
-
 		// let trelloAvatar = "https://trello-avatars.s3.amazonaws.com/\(avatarHash)/30.png"
 		// save the avatar img to the keychain alongside the user token and name
-		// save user as a top lvl coredata obcj w/ token in keychain 
+		// save user as a top lvl coredata obcj w/ token in keychain
 		// - "Store everything in Core Data, except for sensitive information"
+		// does hash change when user pic changes?
 	}
 
-	private func getBoardColors() {
-		//        let trelloBoardColorURL = "https://api.trello.com/1/boards/\(boardID)/prefs?key=\(key)&token=\(user)"
-		// has all logic/processing to store color / custom bk imag into model
+	private func offline(error: NSError?) {
+		println(error)
 	}
 
 // MARK: saves all core data objs for Board
@@ -80,7 +97,7 @@ class TrelloUser {
 //		boardCustomBackground
 //		boardID: String
 //		boardName: String
-//		
+//
 //		relationships ->
 //		lists: NSSet
 //		user: tello.User
